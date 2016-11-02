26f7fd6eca412a392732de271fd8d7443cbf6ab9
Daiwilliam <dgc_william@163.com>
2016-11-03 00:24:27 +0800

    闪光测量

---
+++ app/src/main/java/com/maelook/activity/FlashActivity.java
@@ -0,0 +1,19 @@
+package com.maelook.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.maelook.R;
+/*
+*
+* 闪光测量activity
+*
+* */
+public class FlashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flash);
+    }
+}
