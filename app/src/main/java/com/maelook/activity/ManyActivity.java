ee0ffe3d62e69c027bef09bc51b646a2f529bbd8
Daiwilliam <dgc_william@163.com>
2016-11-03 00:23:49 +0800

    多次测量

---
+++ app/src/main/java/com/maelook/activity/ManyActivity.java
@@ -0,0 +1,17 @@
+package com.maelook.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.maelook.R;
+/*
+* 多次测量activity
+* */
+public class ManyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_many);
+    }
+}
