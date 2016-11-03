abe775a01d86a8c0c1a9cfea2946561d1c453257
Daiwilliam <dgc_william@163.com>
2016-11-03 12:41:06 +0800

    各种动画

--- app/src/main/java/com/maelook/activity/FlashActivity.java
+++ app/src/main/java/com/maelook/activity/FlashActivity.java
@@ -14,6 +14,6 @@ public class FlashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_flash);
+        setContentView(R.layout.activity_measure_fragment);
     }
 }
